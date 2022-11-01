e4f1feb9fa6d5c37c604f8c5adecac275aec2af7
Thomas Kuntz <thomaskuntz67@gmail.com>
2022-11-01 23:18:25 +0100

    Hello world vuejs

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,28 +1,18 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import * as Vue from 'vue'
+import * as VueRouter from 'vue-router'
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+import App from '../App.vue'
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+const myApp = Vue.createApp(App)
 
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
+const myRouter = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [],
+})
 
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+
+myApp.use(myRouter)
+
+document.addEventListener('DOMContentLoaded', () => {
+  myApp.mount('#app')
+})
