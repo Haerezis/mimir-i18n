df49328c2f25011c609c17926e75a11fb535f651
Thomas Kuntz <thomaskuntz67@gmail.com>
2023-07-16 13:47:16 +0200

    add after_initialize to re-generate routes-js files on server startup

--- mimir-i18n-backend/config/initializers/js_routes.rb
+++ mimir-i18n-backend/config/initializers/js_routes.rb
@@ -2,4 +2,9 @@ JsRoutes.setup do |c|
   # Setup your JS module system:
   # ESM, CJS, AMD, UMD or nil
   # c.module_type = "ESM"
+
+  Rails.application.config.after_initialize do
+    JsRoutes.generate!
+    JsRoutes.definitions!
+  end
 end
